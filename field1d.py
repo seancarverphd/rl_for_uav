a974c05c57bee31e555d8d59b86b0060529369c0
Sean Carver <seancarverphd@gmail.com>
2021-04-06 09:02:02 -0400

    Addimg field1d file

---
+++ field1d.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class Field1D():  # To use Gym inherit from gym.Env
+    def __init__(self):
+        self.opt = 5.
+        self.peak = 10.
+        self.left_bound = -10
+        self.right_bound = 10
+        # Objects to define if using gym to pass to solver
+        #    self.observation_space
+        #    self.action_space
+        self.reset()
+
+    def reset(self):
+        self.x = 0
+        self.n = 0  # Count number of steps
+        return self.x
+
+    def step(self, action):
+        assert action == -1 or action == 0 or action == 1
+        self.x += action
+        if self.x < self.left_bound:
+            self.x = self.left_bound
+        if self.x > self.right_bound:
+            self.x = self.right_bound
+        self.n += 1
+        obs = self.x
+        reward = -(self.x - self.opt)**2 + self.peak
+        done = (self.n >= 15)
+        info = {}
+        return obs, reward, done, info
+
+    def render(self):
+        print(str(self.x) + " approaching " + str(self.opt))
+
