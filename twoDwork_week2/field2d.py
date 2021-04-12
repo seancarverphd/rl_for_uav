fdb184583bb0fab384323dd0450278c4cae67f23
Sean Carver <seancarverphd@gmail.com>
2021-04-12 16:49:45 -0400

    Coded field2d.py; need to test

--- twoDwork_week2/field2d.py
+++ twoDwork_week2/field2d.py
@@ -2,7 +2,8 @@ import numpy as np
 
 class Field1D():  # To use Gym inherit from gym.Env
     def __init__(self):
-        self.opt = 5.
+        self.opt_x = 5.
+        self.opt_y = 7.
         self.peak = 10.
         self.left_bound = -20
         self.right_bound = 20
@@ -13,19 +14,33 @@ class Field1D():  # To use Gym inherit from gym.Env
 
     def reset(self):
         self.x = 0
+        self.y = 0
         self.n = 0  # Count number of steps
-        return self.x
+        return self.x, self.y
+
+    def convert_action_2d(self, action):
+        assert isinstance(action, int)
+        assert action >= 0
+        assert action <= 8
+        action_x = action % 3 - 1
+        action_y = int(action / 3) - 1
+        assert action_x == -1 or action_x == 0 or action_x == 1
+        assert action_y == -1 or action_y == 0 or action_y == 1
+
+    def reward_func(self):
+        return -(self.x - self.opt_x)**2 -(self.y - self.opt_y)**2 + self.peak
 
     def step(self, action):
-        assert action == -1 or action == 0 or action == 1
-        self.x += action
+        action_x, action_y = self.convert_action_2d(action)
+        self.x += action_x
+        self.y += action_y
         if self.x < self.left_bound:
             self.x = self.left_bound
         if self.x > self.right_bound:
             self.x = self.right_bound
         self.n += 1
         obs = self.x
-        reward = -(self.x - self.opt)**2 + self.peak
+        reward = self.reward_func()
         done = (self.n >= 15)
         info = {}
         return obs, reward, done, info
