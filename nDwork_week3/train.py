2c07950f0461de6716d7cdfec516ed8fb7a09a1d
Sean Carver <seancarverphd@gmail.com>
2021-04-21 14:36:06 -0400

    More commenting and inconsequential edits

--- nDwork_week3/train.py
+++ nDwork_week3/train.py
@@ -38,7 +38,7 @@ def iterate_batches(env, net, batch_size):
     obs = env.reset()  # should create a flat numpy.array shape=[n_obs]
     softmax = nn.Softmax(dim=1)  # Returns a function
     while True:
-        obs_v = torch.FloatTensor([obs])  # creates a tensor of shape=[1, n_obs]: listification of input needed for shape
+        obs_v = torch.FloatTensor([obs])  # creates a tensor of shape=[1, n_obs]: list from input needed for shape
         act_probs_v = softmax(net(obs_v))  # tensor shape=[1,n_act]: softmax makes them probs of actions
         act_probs = act_probs_v.data.numpy()[0]  # flat numpy.array shape=[n_act]: selecting [0] flattens from 2D-array
         action = np.random.choice(len(act_probs), p=act_probs)  # selects one action from discrete distribution
@@ -59,19 +59,20 @@ def iterate_batches(env, net, batch_size):
 
 
 def filter_batch(batch, percentile):
-    rewards = list(map(lambda s: s.reward, batch))
-    reward_bound = np.percentile(rewards, percentile)
-    reward_mean = float(np.mean(rewards))
+    rewards = list(map(lambda s: s.reward, batch))  # gets a list of rewards for all episodes in batch
+    reward_bound = np.percentile(rewards, percentile)  # computes pth percentile of rewards (p adjustable; eg 70%)
+    reward_mean = float(np.mean(rewards))  # computes mean of rewards 
 
     train_obs = []
     train_act = []
-    for reward, steps in batch:
+    for reward, steps in batch:  # batch is a list of episodes each a named tuple (undiscounted-reward, steps (list-EpisodeStep))
         if reward < reward_bound:
             continue
-        train_obs.extend(map(lambda step: step.observation, steps))
-        train_act.extend(map(lambda step: step.action, steps))
+        train_obs.extend(map(lambda step: step.observation, steps))  # Extend list of all obs with reward above %-ile for this step
+        train_act.extend(map(lambda step: step.action, steps))  # Extend list of all obs with reward above %-ile for this step
+    # ^^^ Each obs is numpy array shape (n_obs,); train_obs is a list of these; likewise for act
 
-    train_obs_v = torch.FloatTensor(train_obs)
+    train_obs_v = torch.FloatTensor(train_obs)  # tensor shape [n_steps_above_threshold, n_obs]
     train_act_v = torch.LongTensor(train_act)
     return train_obs_v, train_act_v, reward_bound, reward_mean
 
@@ -93,8 +94,7 @@ if __name__ == "__main__":
         loss_v = objective(action_scores_v, acts_v)
         loss_v.backward()
         optimizer.step()
-        print("%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f" % (
-            iter_no, loss_v.item(), reward_m, reward_b))
+        print("%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f" % (iter_no, loss_v.item(), reward_m, reward_b))
         writer.add_scalar("loss", loss_v.item(), iter_no)
         writer.add_scalar("reward_bound", reward_b, iter_no)
         writer.add_scalar("reward_mean", reward_m, iter_no)
