31735938054c6484108056dd0c651d83bc17daf8
Sean Carver <seancarverphd@gmail.com>
2021-04-27 13:04:07 -0400

    Removed Commented-Keras-Code on this commit

--- gym-wizards/torch_train_a2c.py
+++ gym-wizards/torch_train_a2c.py
@@ -14,7 +14,8 @@ import numpy as np
 import gym
 import gym_wizards
 
-ENV = "CartPole-v1" # "field1d-v0" # "field2d-v0"
+# ENV = "CartPole-v1" # "field1d-v0" # "field2d-v0"
+ENV = "field1d-v0"
 DEVICE = "cpu"
 HIDDEN_SIZE = 48 # size of hidden layer
 LEARNING_RATE = 0.005
@@ -52,11 +53,8 @@ env = gym.make(ENV)
 obs_size = env.observation_space.shape[0] # 2 # just the x and y positions
 n_actions = env.action_space.n # 9  # kings moves plus stay-in-place
 model = Model(obs_size, n_actions)
-# model = keras.Model(inputs=inputs, outputs=[action, critic])
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-# optimizer = keras.optimizers.Adam(learning_rate=0.005)
 huber_loss = torch.nn.modules.loss.SmoothL1Loss()  # same as torch.nn.HuberLoss() but still available
-# huber_loss = keras.losses.Huber()
 
 action_probs_history = []
 critic_value_history = []
@@ -66,43 +64,28 @@ best = None
 for episode in range(EPISODES):
     state = env.reset()
     episode_reward = 0
-    # with tf.GradientTape(persistent=False) as tape:
-    # step = 0  # TODO Uncomment for while not done
     # done = False  # TODO Uncomment for while not done
     # while not done:  # better to let the environment count the steps as with some problems the number can be variable 
     for step in range(STEPS_PER_EPISODE):
-
         # Take a step using the learned policy
         state_v = torch.FloatTensor([state])  # TODO comment still good?:  creates a tensor of shape=[1, n_obs]: list from input needed for shape
         state_v = state_v.to(DEVICE)
-        # curr_st_array = tf.convert_to_tensor(state)
-        # curr_st_array = tf.expand_dims(curr_st_array, 0)
         action_probs_v, critic_value_v = model(state_v)
         action_probs = action_probs_v.squeeze(dim=0).data.cpu().numpy()  # TODO is the squeeze necessary?
         critic_value = critic_value_v.squeeze(dim=0).data.cpu().numpy()  # TODO is the squeeze necessary?
-
-        # action_probs, critic_value = model(curr_st_array)
         critic_value_history.append(critic_value_v)
-        # critic_value_history.append(critic_value[0, 0])  TODO is subscipting still necessary
-
-        # sample action from probability distribution
-        action = np.random.choice(n_actions, p=np.squeeze(action_probs))  # TODO squeeze necessary?
+        action = np.random.choice(n_actions, p=np.squeeze(action_probs))  # sample action from probability distribution  # TODO squeeze necessary?
         action_probs_history.append(math.log(action_probs[action]))  # TODO indexing correct?
-
         state, reward, done, _ = env.step(action)
-
         rewards_history.append(reward)
         episode_reward += reward
-        # step += 1  # TODO Uncomment for while not done
-    
-    ### FINISHED ONE EPISODE NOW PROCESS THAT EPISODE
 
+    ### FINISHED ONE EPISODE NOW PROCESS THAT EPISODE
     if best is None or best < episode_reward:
         best = episode_reward
         beststr = "BEST"
     else:
         beststr = ''
-
     print(state, "Cumulative reward:", episode_reward, beststr) # examine the final state of each episode plus more
 
     # store the cumulative, discounted rewards
@@ -113,15 +96,15 @@ for episode in range(EPISODES):
         cumulative_discounted_rewards.insert(0, discounted_sum)
 
     # Normalize the cumulative, discounted reward history
+    # TODO  Test.  I am suspicous of the next three lines.  Better, I think, to standardize batches than episodes
     cumulative_discounted_rewards = np.array(cumulative_discounted_rewards)
-    # TODO  Test.  I am suspicous of the next two lines.  Better, I think, to standardize batches than episodes
     normalized_cumulative_discounted_rewards = (cumulative_discounted_rewards - np.mean(cumulative_discounted_rewards)) / (np.std(cumulative_discounted_rewards) + 0.000001)
     normalized_cumulative_discounted_rewards = normalized_cumulative_discounted_rewards.tolist()
 
     history = zip(action_probs_history, critic_value_history, normalized_cumulative_discounted_rewards)
     actor_losses = []
     critic_losses = []
-    optimizer.zero_grad()
+    optimizer.zero_grad()  # TODO Is this where it needs to be?
     for log_prob, critic_val, normed_cum_disc_rew in history:
         # At this point in history, the critic estimated that we would get a
         # total reward = `value` in the future. We took an action with log probability
@@ -131,49 +114,27 @@ for episode in range(EPISODES):
         diff = normed_cum_disc_rew - critic_val
         actor_losses.append(-log_prob * diff)  # actor loss
 
-        # The critic must be updated so that it predicts a better estimate of
-        # the future rewards.
+        # The critic must be updated so that it predicts a better estimate of the future rewards.
         critic_losses.append(huber_loss(critic_val, torch.FloatTensor([normed_cum_disc_rew])))  # TODO Probably need to massage dimensions but I don't know how)
-        # critic_losses.append(
-        #    huber_loss(tf.expand_dims(critic_val, 0), tf.expand_dims(normed_cum_disc_rew, 0))
-        # )
-        # NEW: loss_value_v = huber_loss(critic_v.unsqueeze(dim=-1), vals_ref_v)
-        '''OLD OLD OLD
-        adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
-        log_prob_v = adv_v * calc_logprob(mu_v, var_v, actions_v)
-        loss_policy_v = -log_prob_v.mean()
-        ent_v = -(torch.log(2*math.pi*var_v) + 1)/2
-        entropy_loss_v = ENTROPY_BETA * ent_v.mean()
-
-        loss_v = loss_policy_v + entropy_loss_v + loss_value_v
-        loss_v.backward()
-        optimizer.step()
-
-
+        '''======= OLD ============
         optimizer.zero_grad()
         mu_v, var_v, value_v = net(states_v)
-
         loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
-
         adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
         log_prob_v = adv_v * calc_logprob(mu_v, var_v, actions_v)
         loss_policy_v = -log_prob_v.mean()
         ent_v = -(torch.log(2*math.pi*var_v) + 1)/2
         entropy_loss_v = ENTROPY_BETA * ent_v.mean()
-
         loss_v = loss_policy_v + entropy_loss_v + loss_value_v
         loss_v.backward()
         optimizer.step()'''
-    # TODO Incredulous that this will work
+
     # Backpropagation
     overall_loss_value = sum(actor_losses) + sum(critic_losses)
     overall_loss_value.backward()
-    # gradientss = tape.gradient(overall_loss_value, model.trainable_variables)
-    
     optimizer.step()
-    # optimizer.apply_gradients(zip(gradientss, model.trainable_variables))
 
-    # Clear the loss and reward history for next episode
+    # Reset env and clear the loss and reward history for next episode
     action_probs_history.clear()
     critic_value_history.clear()
     rewards_history.clear()
