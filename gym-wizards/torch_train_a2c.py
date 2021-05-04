5f7a448227bb2ba164ccb63f445ed3de253b66a7
Sean Carver <seancarverphd@gmail.com>
2021-05-04 18:11:42 -0400

    Fixed problem of not differentiating Actor

--- gym-wizards/torch_train_a2c.py
+++ gym-wizards/torch_train_a2c.py
@@ -8,11 +8,11 @@ import gym
 import gym_wizards
 
 # ENV = "CartPole-v1" # "field1d-v0" # "field2d-v0"
-ENV = "field1d-v0"
+ENV = "field2d-v0"
 DEVICE = "cpu"
 HIDDEN_SIZE = 48 # size of hidden layer
 LEARNING_RATE = 0.005
-EPISODES = 500  # Really this is number of singleton batches
+EPISODES = 5000  # Really this is number of singleton batches
 STEPS_PER_EPISODE = 30  # Does not apply to CartPole (variable)
 SET_STEPS = True  # True if environment has a self.max_steps attribute and you want to set it to STEPS_PER_EPISODE
 GAMMA = .9  # Discount factor for rewards
@@ -78,7 +78,7 @@ class Agent():
             state, reward, done, _ = self.env.step(action)
             rewards_history.append(reward)
             episode_reward += reward
-        return state, episode_reward, rewards_history, action_logprobs_history, critic_value_history, critic_value_v_history
+        return state, episode_reward, rewards_history, action_logprobs_history, action_logprobs_v_history, critic_value_history, critic_value_v_history
 
 
     def proc_best(self, episode_reward):
@@ -88,8 +88,8 @@ class Agent():
         else:
             return ''
 
-    def print_episode_stats(self, final_state, episode_reward, best_episode_so_far_str):
-        print(final_state, "Cumulative reward:", episode_reward, best_episode_so_far_str) # examine the final state of each episode plus more
+    def print_episode_stats(self, n, final_state, episode_reward, best_episode_so_far_str):
+        print(n, final_state, "Cumulative reward:", episode_reward, best_episode_so_far_str) # examine the final state of each episode plus more
 
     def discount_and_standardize(self, rewards_history):
         # store the cumulative, discounted rewards
@@ -104,19 +104,19 @@ class Agent():
 
     def batch(self):
         self.best = None  # self.best will be best episode over all episodes in batch
-        for _ in range(EPISODES):
+        for n in range(EPISODES):
             self.optimizer.zero_grad()  # TODO Is this where it needs to be?
 
-            final_state, episode_reward, rewards_history, action_logprobs_history, critic_value_history, critic_value_v_history = self.episode()
+            final_state, episode_reward, rewards_history, action_logprobs_history, action_logprobs_v_history, critic_value_history, critic_value_v_history = self.episode()
 
             ### FINISHED ONE EPISODE NOW PROCESS THAT EPISODE
             best_episode_so_far_str = self.proc_best(episode_reward)
-            self.print_episode_stats(final_state, episode_reward, best_episode_so_far_str)
+            self.print_episode_stats(n, final_state, episode_reward, best_episode_so_far_str)
             transformed_rewards = self.discount_and_standardize(rewards_history)
 
             # Normalize the cumulative, discounted reward history
 
-            history = zip(action_logprobs_history, critic_value_v_history, transformed_rewards)
+            history = zip(action_logprobs_v_history, critic_value_v_history, transformed_rewards)
             actor_losses = []
             critic_losses = []
             for log_prob, critic_val, transformed_reward in history:
