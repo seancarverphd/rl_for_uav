9557f6e32021d006d492569379cbf163947f31da
Sean Carver <seancarverphd@gmail.com>
2021-05-04 14:24:10 -0400

    Lots of code modifications

--- gym-wizards/torch_train_a2c.py
+++ gym-wizards/torch_train_a2c.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
-# adapted wholesale from https://keras.io/examples/rl/actor_critic_cartpole/
-# then adapted for PyTorch
+# adapted wholesale from https://keras.io/examples/rl/actor_critic_cartpole/ then adapted for PyTorch
 
-# imports
-# import tensorflow_probability as tfp
-# tfd = tfp.distributions
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras import layers
 import torch
 import math
 import numpy as np
@@ -24,8 +17,8 @@ STEPS_PER_EPISODE = 30  # Does not apply to CartPole (variable)
 SET_STEPS = True  # True if environment has a self.max_steps attribute and you want to set it to STEPS_PER_EPISODE
 GAMMA = .9  # Discount factor for rewards
 
-## CREATE THE NEURAL NETWORK
 
+## CREATE THE NEURAL NETWORK
 class Model(torch.nn.Module):
     def __init__(self, obs_size, n_actions):
         super(Model, self).__init__()
@@ -96,54 +89,50 @@ class Agent():
     def print_episode_stats(self, final_state, episode_reward, best_episode_so_far_str):
         print(final_state, "Cumulative reward:", episode_reward, best_episode_so_far_str) # examine the final state of each episode plus more
 
+    def discount_and_standardize(self, rewards_history):
+        # store the cumulative, discounted rewards
+        cumulative_discounted_rewards = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + GAMMA * discounted_sum
+            cumulative_discounted_rewards.insert(0, discounted_sum)
+        cumulative_discounted_rewards = np.array(cumulative_discounted_rewards)
+        normalized_cumulative_discounted_rewards = (cumulative_discounted_rewards - np.mean(cumulative_discounted_rewards)) / (np.std(cumulative_discounted_rewards) + 0.000001)
+        return normalized_cumulative_discounted_rewards.tolist()
+
     def batch(self):
         self.best = None  # self.best will be best episode over all episodes in batch
         for _ in range(EPISODES):
             self.optimizer.zero_grad()  # TODO Is this where it needs to be?
 
             final_state, episode_reward, rewards_history, action_probs_history, critic_value_history, critic_value_v_history = self.episode() 
+
             ### FINISHED ONE EPISODE NOW PROCESS THAT EPISODE
             best_episode_so_far_str = self.proc_best(episode_reward)
             self.print_episode_stats(final_state, episode_reward, best_episode_so_far_str)
-
-            # store the cumulative, discounted rewards
-            cumulative_discounted_rewards = []
-            discounted_sum = 0
-            for r in rewards_history[::-1]:
-                discounted_sum = r + GAMMA * discounted_sum
-                cumulative_discounted_rewards.insert(0, discounted_sum)
+            transformed_rewards = self.discount_and_standardize(rewards_history)
 
             # Normalize the cumulative, discounted reward history
-            cumulative_discounted_rewards = np.array(cumulative_discounted_rewards)
-            normalized_cumulative_discounted_rewards = (cumulative_discounted_rewards - np.mean(cumulative_discounted_rewards)) / (np.std(cumulative_discounted_rewards) + 0.000001)
-            normalized_cumulative_discounted_rewards = normalized_cumulative_discounted_rewards.tolist()
 
-            history = zip(action_probs_history, critic_value_v_history, normalized_cumulative_discounted_rewards)
+            history = zip(action_probs_history, critic_value_v_history, transformed_rewards)
             actor_losses = []
             critic_losses = []
-            for log_prob, critic_val, normed_cum_disc_rew in history:
-                # At this point in history, the critic estimated that we would get a
-                # total reward = `value` in the future. We took an action with log probability
-                # of `log_prob` and ended up recieving a total reward = `ret`.
-                # The actor must be updated so that it predicts an action that leads to
-                # high rewards (compared to critic's estimate) with high probability.
-                diff = normed_cum_disc_rew - critic_val
+            for log_prob, critic_val, transformed_reward in history:
+                '''At this point in history
+                        the critic estimated that we would get a # total reward = `critic_val` in the future.
+                        We took an action with log probability `log_prob` 
+                        and ended up recieving a total reward = `transformed_reward`.
+                   The actor must be updated so that it predicts an action that leads to high rewards (compared to critic's estimate) with high probability.'''
+                diff = transformed_reward - critic_val
                 actor_losses.append(-log_prob * diff)  # actor loss
 
                 # The critic must be updated so that it predicts a better estimate of the future rewards.
-                critic_losses.append(self.huber_loss(critic_val[0][0], torch.FloatTensor([normed_cum_disc_rew])))
-                '''======= OLD ============
-                self.optimizer.zero_grad()
-                mu_v, var_v, value_v = net(states_v)
-                loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
-                adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
-                log_prob_v = adv_v * calc_logprob(mu_v, var_v, actions_v)
-                loss_policy_v = -log_prob_v.mean()
+                critic_losses.append(self.huber_loss(torch.FloatTensor(critic_val[0][0]), torch.FloatTensor([transformed_reward])))
+
+                '''======= FROM LAPIN ============
                 ent_v = -(torch.log(2*math.pi*var_v) + 1)/2
                 entropy_loss_v = ENTROPY_BETA * ent_v.mean()
-                loss_v = loss_policy_v + entropy_loss_v + loss_value_v
-                loss_v.backward()
-                self.optimizer.step()'''
+                loss_v = loss_policy_v + entropy_loss_v + loss_value_v'''
 
             # Backpropagation
             overall_loss_value = sum(actor_losses) + sum(critic_losses)
