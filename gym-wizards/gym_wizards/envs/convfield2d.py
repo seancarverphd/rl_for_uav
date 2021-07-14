62cf187eb606383e85dd6fb56b6d6a764488dba2
Sean Carver <seancarverphd@gmail.com>
2021-07-14 18:34:52 -0400

    Updated environment for convnet

--- gym-wizards/gym_wizards/envs/convfield2d.py
+++ gym-wizards/gym_wizards/envs/convfield2d.py
@@ -15,27 +15,28 @@ class Grid():
     def add_xy(self, x, y):
         x1 = int(round(min(x/self.dx, self.nx-1)))
         y1 = int(round(min(y/self.dy, self.ny-1)))
-        print(x1, y1)
         self.grid[y1,x1] += 1
-        print(self.grid)
 
 class Field2D(gym.Env):  # To use Gym inherit from gym.Env
     def __init__(self):
         self.opt_x = 5.
         self.opt_y = 7.
         self.peak = 10.
-        max_steps = 30
+        self.max_steps = 30
         self.left_bound = -20
         self.right_bound = 20
         self.lower_bound = -20
         self.upper_bound = 20
         self.observation_space = gym.spaces.Box(low=np.array((self.left_bound,self.lower_bound), dtype=np.float32), high=np.array((self.right_bound,self.upper_bound), dtype=np.float32))
         self.action_space = gym.spaces.Discrete(9)  # king's moves plus stay
+        self.comms = Grid()
         self.reset()
 
     def reset(self):
         self.x = 0
         self.y = 0
+        self.comms.clear_grid()
+        self.comms.add_xy(self.x,self.y)
         self.n = 0  # Count number of steps
         return [self.x, self.y]
 
@@ -65,12 +66,14 @@ class Field2D(gym.Env):  # To use Gym inherit from gym.Env
         if self.y > self.upper_bound:
             self.y = self.upper_bound
         self.n += 1
-        obs = [self.x, self.y]
+        self.comms.clear_grid()
+        self.comms.add_xy(self.x,self.y)
+        obs = self.comms.grid 
         reward = self.reward_func()
         done = (self.n >= self.max_steps)
         info = {}
         return obs, reward, done, info
 
     def render(self):
-        print("["+ str(self.x) + ", "+ str(self.y) + "] approaching [" + str(self.opt_x) + ", " + str(self.opt_y) + "]")
-
+        print("Approaching [" + str(self.opt_x) + ", " + str(self.opt_y) + "]")
+        print(self.comms.grid)
